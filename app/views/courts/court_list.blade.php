2a065f44d6c4d670ecf0fcb4334bae1eea6bd076
redsh1ftr <dev@precisemaintenance.com>
2014-04-17 09:09:10 -0400

    Cosmetics
    Fixed the court select for new cases for pretties.

--- app/views/courts/court_list.blade.php
+++ app/views/courts/court_list.blade.php
@@ -16,13 +16,9 @@
 <?php $court_link_county = $court_info->county; ?>
 
 @if($court_link_type === 'Federal')
-<ul>
 {{link_to_route('court_profile', "$court_link_number United States $court_link_type Court", $court_info->id, array('id' => $court_info->id)); }}
-</ul>
 @else
-<ul>
 {{link_to_route('court_profile', "$court_link_number $court_link_type Court of $court_link_county County", $court_info->id, array('id' => $court_info->id)); }}
-</ul>
 @endif
 
 @endforeach
