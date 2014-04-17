19052184f1f1b50c0bb0113a570bfac8cb0beadb
redsh1ftr <dev@precisemaintenance.com>
2014-04-17 18:02:55 -0400

    Courts Cases and Cosmetics
    Fixed some buggy stuff, added a right container for jobs/attorneys etc.

--- app/views/cases/select_court.blade.php
+++ app/views/cases/select_court.blade.php
@@ -7,14 +7,10 @@
 <?php $court_link_county = $court_info->county; ?>
 
 @if($court_link_type === 'Federal')
-{{link_to_route('court_profile', "$court_link_number United States $court_link_type Court", $court_info->id, array('id' => $court_info->id)); }}
+{{link_to_route('selected_court', "$court_link_number United States $court_link_type Court", $court_info->id, array('id' => $court_info->id)); }}
 @else
-{{link_to_route('court_profile', "$court_link_number $court_link_type Court of $court_link_county County", $court_info->id, array('id' => $court_info->id)); }}
+{{link_to_route('selected_court', "$court_link_number $court_link_type Court of $court_link_county County", $court_info->id, array('id' => $court_info->id)); }}
 @endif
 
 @endforeach
-
-<h2>
-	{{link_to_route('new_court', 'New Court') }}
-</h2>
 @stop
