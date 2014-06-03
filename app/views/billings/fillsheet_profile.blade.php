2f6cc22928f6a1e3f8a3a54ed6a800af1841a70d
redsh1ftr <dev@precisemaintenance.com>
2014-06-03 17:02:06 -0400

    Billing
    Added bill requester basics

--- app/views/billings/fillsheet_profile.blade.php
+++ app/views/billings/fillsheet_profile.blade.php
@@ -1,14 +1,25 @@
 @extends('layouts.profile')
 @section('content_left')
+
+{{-- VARIABLE DECLARATIONS --}}
+
 <?php $deponentname = DeponentMain::where('id', '=', JobMain::where('id', '=', $cost->job)->pluck('deponent_id'))->pluck('name');?>
 <?php $nor = NorMain::where('id', '=', JobMain::where('id', '=', $cost->job)->pluck('nor_id'))->first();?>
 <?php $shipcost = AttorneyMain::where('id', '=', $cost->requester)->pluck('shipping');?>
+<?php $invv = number_format(CompanyDefault::first()->subp_fee + $cost->invoices + $cost->cost + $shipcost, 2);?>
+
+{{-- END VARIABLES --}}
+
+
+
+
+
 <table width="120%" border="1">
 
 <td>YOUR FILE# {{CaseMain::where('id', '=', JobMain::where('id', '=', $cost->job)->pluck('case_id'))->pluck('claim_number')}}<tr>
 @if($cost->subp)
 <td>Preparation of Subpoena and Notice regarding {{$deponentname}}
-	<td> ${{$cost->subp}}<tr>
+<td> ${{$cost->subp}}<tr>
 @endif
 <td>Facility Fee<td>${{number_format($cost->invoices, 2)}}<tr>
 <td>Reproduction of Records received from Deponent {{$deponentname}} regarding {{$nor->first_name}} {{$nor->middle_name}} {{$nor->last_name}} 
@@ -17,12 +28,22 @@
 
 <td>Postage and Handling.<td>${{number_format($shipcost,2)}}<tr>
 
+
+
 </table>
 <br><br>
 <table width="120%" border="0">
 <td>Invoice Total:<td>${{number_format(CompanyDefault::first()->subp_fee + $cost->invoices + $cost->cost + $shipcost, 2)}}
 </table>
 
+{{ Form::open(array('route' => 'add_records', 'POST')) }}
+<table>
+<td>
+{{Form::text('cost', $invv)}}
+<tr>
 
+{{Form::submit('Create Invoice')}}
+
+{{Form::close()}}
 
 @stop
